365f10fd902d70f55f5a88d6721117aef0ae2db4
pramodgithub <pramodvitpune@gmail.com>
2022-07-27 09:06:10 +0530

    added legacy store

--- client/src/App.js
+++ client/src/App.js
@@ -1,42 +1,49 @@
 import React, { useEffect } from "react";
-import { Container, AppBar, Typography, Grow, Grid } from '@material-ui/core';
+import { Container, AppBar, Typography, Grow, Grid } from "@material-ui/core";
 import { useDispatch } from "react-redux";
 
-import { getPosts } from './actions/posts'
+import { getPosts } from "./actions/posts";
 
-import logo20  from './images/logo20.png'
+import logo20 from "./images/logo20.png";
 import Posts from "./components/Posts/Posts";
 import Form from "./components/Form/Form";
-import useStyles from "./styles"
+import useStyles from "./styles";
 
 const App = () => {
-    const classes = useStyles();
-    const dispatch = useDispatch();
+  const classes = useStyles();
+  const dispatch = useDispatch();
 
-    useEffect(() => {
-      dispatch(getPosts())
-    },[dispatch]); 
+  useEffect(() => {
+    dispatch(getPosts());
+  }, [dispatch]);
 
-    return(
-        <Container maxWidth="lg">
+  return (
+    <Container maxWidth="lg">
       <AppBar className={classes.appBar} position="static" color="inherit">
-        <Typography className={classes.heading} variant="h2" align="center">Memories</Typography>
+        <Typography className={classes.heading} variant="h2" align="center">
+          Memories
+        </Typography>
         <img className={classes.image} src={logo20} alt="icon" height="60" />
       </AppBar>
       <Grow in>
         <Container>
-          <Grid container justify="space-between" alignItems="stretch" spacing={3}>
+          <Grid
+            container
+            justify="space-between"
+            alignItems="stretch"
+            spacing={3}
+          >
             <Grid item xs={12} sm={7}>
               <Posts />
             </Grid>
             <Grid item xs={12} sm={4}>
-              <Form  />
+              <Form />
             </Grid>
           </Grid>
         </Container>
       </Grow>
-      </Container>
-    )
-}
+    </Container>
+  );
+};
 
 export default App;
--- client/src/index.js
+++ client/src/index.js
@@ -1,20 +1,23 @@
-import React from 'react'
-import ReactDOM  from 'react-dom'
-import { Provider } from 'react-redux';
-import { legacy_createStore as createStore , applyMiddleware, compose } from 'redux';
+import React from "react";
+import ReactDOM from "react-dom";
+import { Provider } from "react-redux";
+import {
+  legacy_createStore as createStore,
+  applyMiddleware,
+  compose,
+} from "redux";
 
-import thunk from 'redux-thunk'
+import thunk from "redux-thunk";
 
-import { reducers } from './reducers' 
+import { reducers } from "./reducers";
 
-
-import App from './App.js'
+import App from "./App.js";
 
 const store = createStore(reducers, compose(applyMiddleware(thunk)));
 
 ReactDOM.render(
-    <Provider store={store}>
-        <App />
-    </Provider>, 
-    document.getElementById('root'));
-
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById("root")
+);
